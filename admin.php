5abc87216f77b6d36ecf4b56e2652092b97a520f
Maria <maryika93@mail.ru>
2018-04-30 22:01:48 +0600

    300418

--- admin.php
+++ admin.php
@@ -3,49 +3,10 @@ require_once 'vendor/autoload.php';
 require_once 'model/User.php';
 require_once 'lib/connect.php';
 
-try {
-    $db       = db();
-    if (!empty($_POST)) {
-        if (isset($_POST['inp'])) {
-            if (isset($_POST['login'])) {
-                $login = $_POST['login'];
-            }
-            if (isset($_POST['password'])) {
-                $password = md5($_POST['password']);
-            }
+if (!isset($_POST['inp'])) {
+    include 'controller/auth.php';
+}
 
-            $data = selectAllUsers($login);
-
-            if (empty($data[0]['pass'])) {
-                $err = "Извините, введённый вами логин или пароль неверный";
-            } else {
-                if ($data[0]['pass'] === $password) {
-                    $_SESSION['login'] = $data[0]['login'];
-                    $_SESSION['id']    = $data[0]['id'];
-                } else {
-                    $err = "Извините, введённый вами логин или пароль неверный";
-                }
-            }
-        }
-    }
-
-    if ((isset($_SESSION)) || ((isset($_GET['c'])) && (($_GET['c'] == 'back')))) {
-        $loader   = new Twig_Loader_Filesystem('templates');
-        $twig     = new Twig_Environment($loader, array(
-            'cache' => 'cache',
-            'auto_reload' => true));
-        $template = $twig->loadTemplate('admin_index.twig');
-        echo $twig->render($template);
-    }
-    else{
-        $loader   = new Twig_Loader_Filesystem('templates');
-        $twig     = new Twig_Environment($loader, array(
-            'cache' => 'cash',
-            'auto_reload' => true));
-        $err      = "";
-        $template = $twig->loadTemplate('admin.twig');
-        echo $twig->render($template, array('err' => $err));
-    }
         if (isset($_GET['c'])) {
             if ($_GET['c'] == 'administrators') {
                 include 'controller/administrators.php';
@@ -57,13 +18,4 @@ try {
                 include 'controller/editCategory.php';
             }
         }
-
-}
-
-catch(PDOException $e)
-{
-    die("Error: " . $e->getMessage());
-}
-
-
 ?>
---
+++ controller/auth.php
@@ -0,0 +1,53 @@
+<?php
+require_once 'vendor/autoload.php';
+require_once 'model/User.php';
+require_once 'lib/connect.php';
+
+try {
+    $db = db();
+    if (!empty($_POST)) {
+        if (isset($_POST['inp'])) {
+            if (isset($_POST['login'])) {
+                $login = $_POST['login'];
+            }
+            if (isset($_POST['password'])) {
+                $password = md5($_POST['password']);
+            }
+
+            $data = selectAllUsers($login);
+
+            if (empty($data[0]['pass'])) {
+                $err = "Извините, введённый вами логин или пароль неверный";
+            } else {
+                if ($data[0]['pass'] === $password) {
+                    $_SESSION['login'] = $data[0]['login'];
+                    $_SESSION['id']    = $data[0]['id'];
+                } else {
+                    $err = "Извините, введённый вами логин или пароль неверный";
+                }
+            }
+        }
+    }
+
+    if ((isset($_SESSION['login'])) || ((isset($_GET['c'])) && (($_GET['c'] == 'back')))) {
+        $loader   = new Twig_Loader_Filesystem('templates');
+        $twig     = new Twig_Environment($loader, array(
+            'cache' => 'cache',
+            'auto_reload' => true));
+        $template = $twig->loadTemplate('admin_index.twig');
+        echo $twig->render($template);
+    } else {
+        $loader   = new Twig_Loader_Filesystem('templates');
+        $twig     = new Twig_Environment($loader, array(
+            'cache' => 'cash',
+            'auto_reload' => true));
+        $err      = "";
+        $template = $twig->loadTemplate('admin.twig');
+        echo $twig->render($template, array('err' => $err));
+    }
+}
+catch(PDOException $e)
+{
+    die("Error: " . $e->getMessage());
+}
+?>
